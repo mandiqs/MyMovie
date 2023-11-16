01804bae70b91d4fb01579cbacd34f8379d842db
mandiqs <136293700+mandiqs@users.noreply.github.com>
2023-11-16 00:38:01 -0300

    Versão final - Filme

--- src/entidades/Filme.java
+++ src/entidades/Filme.java
@@ -5,7 +5,7 @@ public class Filme extends Video {
 	private int duracao;
 
 	public Filme(String nome, double nota, Genero genero, int duracao) {
-		super(nome, nota, genero.getNome());
+		super(nome, nota, genero);
 		this.duracao = duracao;
 	}
 
@@ -15,6 +15,17 @@ public class Filme extends Video {
 
 	public void setDuracao(int duracao) {
 		this.duracao = duracao;
+	}
+
+	@Override
+	public void reproduzir() {
+		System.out.println("Reproduzindo o Filme");
+	}
+
+	@Override
+	public void exibirDetalhes() {
+		super.exibirDetalhes();
+		System.out.println("Duração do Filme: " + duracao);
 	}
 
 	@Override
