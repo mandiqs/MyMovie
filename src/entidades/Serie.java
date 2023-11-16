d280928559485947073bef8acbaae7d43c8d672a
carloshobmeier <130938157+carloshobmeier@users.noreply.github.com>
2023-11-16 00:47:38 -0300

    Versão final - série

--- src/entidades/Serie.java
+++ src/entidades/Serie.java
@@ -5,7 +5,7 @@ public class Serie extends Video {
     private int duracao;
 
     public Serie(String nome, double nota, Genero genero, int duracao) {
-        super(nome, nota, genero.getNome());
+        super(nome, nota, genero);
         this.duracao = duracao;
     }
 
@@ -15,6 +15,17 @@ public class Serie extends Video {
 
     public void setDuracao(int duracao) {
         this.duracao = duracao;
+    }
+
+    @Override
+    public void reproduzir() {
+        System.out.println("Reproduzindo a Série");
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Duração do Episódio: " + duracao);
     }
 
     @Override
