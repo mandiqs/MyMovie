26b86c7d5902980484a54a2adeb5cb649ebfb5d9
carloshobmeier <130938157+carloshobmeier@users.noreply.github.com>
2023-11-16 00:52:07 -0300

    Versão final tela inicial

--- src/telas/TelaInicialFrame.java
+++ src/telas/TelaInicialFrame.java
@@ -1,9 +1,11 @@
 package telas;
 
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.LineBorder;
 
 import aplicacao.Programa;
 
@@ -15,6 +17,8 @@ public class TelaInicialFrame extends JFrame {
     private JLabel myLabelSeries;
     private JLabel myLabelMain;
     private ImageIcon mainIcon;
+    private JLabel MyLabelLogo;
+    private ImageIcon logoIcon;
     private JLabel myLabelGeneros;
     private ImageIcon generosIcon;
 
@@ -22,12 +26,53 @@ public class TelaInicialFrame extends JFrame {
         super(title);
         this.app = app;
 
-        // Configurar os componentes da interface gráfica
+        // Componentes da interface gráfica
+        JButton logo = new JButton("|");
         JButton cadastrarFilmesButton = new JButton("Cadastrar Filmes");
         JButton cadastrarGenerosButton = new JButton("Cadastrar Gêneros");
-        JButton cadastrarSeriesButton = new JButton("Cadastrar Séries"); // Novo botão para cadastrar séries
+        JButton cadastrarSeriesButton = new JButton("Cadastrar Séries");
 
-        // Layout
+        // Aumentando o tamanho dos botões
+        Dimension tamanhoBotao = new Dimension(180, 40);
+        logo.setPreferredSize(new Dimension(25,30));
+        cadastrarFilmesButton.setPreferredSize(tamanhoBotao);
+        cadastrarGenerosButton.setPreferredSize(tamanhoBotao);
+        cadastrarSeriesButton.setPreferredSize(tamanhoBotao);
+
+
+        // Gambiarra para tirar a 'caixa' que envolvia o texto do botão 'cadastrar filmes
+        logoIcon = new ImageIcon(this.getClass().getResource("/recursos/popcorn.png"));
+        MyLabelLogo = new JLabel(logoIcon);
+        MyLabelLogo.setSize(51, 51);
+        logo.setBackground(new Color(3,5,54));
+        logo.setBorder(null);
+        logo.setIcon(logoIcon);
+
+        // Colocando cor nos botões
+        cadastrarFilmesButton.setBackground(new Color(242, 18, 78));
+        cadastrarGenerosButton.setBackground(new Color(242, 18, 78));
+        cadastrarSeriesButton.setBackground(new Color(242, 18, 78));
+
+        // Colocando cor nas letras dos botões
+        cadastrarFilmesButton.setForeground(Color.white);
+        cadastrarGenerosButton.setForeground(Color.white);
+        cadastrarSeriesButton.setForeground(Color.white);
+
+        // Alterando a fonte dos botões
+        Font fonteBotao = new Font("Lucida Fax", Font.BOLD, 16);
+        cadastrarFilmesButton.setFont(fonteBotao);
+        cadastrarGenerosButton.setFont(fonteBotao);
+        cadastrarSeriesButton.setFont(fonteBotao);
+
+        // Colocando bordas nos botões
+        cadastrarFilmesButton.setBorder(new LineBorder(Color.WHITE, 4));
+        cadastrarSeriesButton.setBorder(new LineBorder(Color.WHITE, 4));
+        cadastrarGenerosButton.setBorder(new LineBorder(Color.WHITE, 4));
+
+        // Cor no plano de fundo
+        getContentPane().setBackground(new Color(3,5,54));
+
+
         filmesIcon = new ImageIcon(this.getClass().getResource("/recursos/imagem-filmes.jpg"));
         myLabelFilmes = new JLabel(filmesIcon);
         myLabelFilmes.setSize(650, 650);
@@ -45,8 +90,9 @@ public class TelaInicialFrame extends JFrame {
         myLabelGeneros.setSize(650, 450);
 
         setLayout(new FlowLayout());
+        add(logo);
         add(cadastrarFilmesButton);
-        add(cadastrarSeriesButton);// Adicionar o botão para cadastrar séries
+        add(cadastrarSeriesButton);
         add(cadastrarGenerosButton);
 
         cadastrarFilmesButton.addActionListener(new ActionListener() {
@@ -66,13 +112,13 @@ public class TelaInicialFrame extends JFrame {
         cadastrarSeriesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                abrirTelaCadastroSeries(); // Ação para abrir a tela de cadastro de séries
+                abrirTelaCadastroSeries();
             }
         });
 
         setSize(660, 710);
         add(myLabelMain);
-        Programa.centerFrame(this); // Centraliza a janela
+        Programa.centerFrame(this);
     }
 
     private void abrirTelaCadastroFilmes() {
@@ -80,7 +126,7 @@ public class TelaInicialFrame extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.add(myLabelFilmes);
-        Programa.centerFrame(frame); // Centraliza a janela
+        Programa.centerFrame(frame);
         frame.setVisible(true);
         this.setVisible(false);
     }
@@ -90,17 +136,17 @@ public class TelaInicialFrame extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.add(myLabelGeneros);
-        Programa.centerFrame(frame); // Centraliza a janela
+        Programa.centerFrame(frame);
         frame.setVisible(true);
         this.setVisible(false);
     }
 
     private void abrirTelaCadastroSeries() {
-        JFrame frame = new CadastroDeSeriesFrame("Cadastro de Séries", app); // Substitua "CadastroDeSeriesFrame" pelo nome correto da classe de cadastro de séries
+        JFrame frame = new CadastroDeSeriesFrame("Cadastro de Séries", app);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.add(myLabelSeries);
-        Programa.centerFrame(frame); // Centraliza a janela
+        Programa.centerFrame(frame);
         frame.setVisible(true);
         this.setVisible(false);
     }
